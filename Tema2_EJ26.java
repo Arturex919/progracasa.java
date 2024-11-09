318aae8bf64e35d809aad099d5378a6784aac417
Arturex919 <edutuacal@alu.edu.gva.es>
2024-11-09 14:59:41 +0100

    29/10

--- Tema2_EJ26.java
+++ Tema2_EJ26.java
@@ -7,22 +7,28 @@ import java.util.Scanner;
 public class Tema2_EJ26 {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        int a,b,cont=0;
-        int aux;
-        System.out.println("introduzca un numero");
-        a=in.nextInt();
-        for (b=1; b<=a;b++){
-            aux=b%a;
-        }
-        if (cont<=2){
-            System.out.println("el numero es primo");
+        int num,divi;
+        System.out.println("ingrese un numero");
+        num=in.nextInt();
+        //vemos si el numero que metemos es igual que uno//
+        // si es igual a uno no es primo porque solo tiene un divisor//
+        if (num==1){
+            System.out.println("no es primo");
         }else{
-            System.out.println("el numero no es primo");
-
+            divi=2;
+            while (num%divi!=0){
+                divi++;
+                //entonces dividimos el numero que ingresamos para que este vaya sacando//
+                // los numero que son primos mientras se va incrementando el valor//
+            }
+            if (divi==num){
+                System.out.println("su numero es primo");
+            }else{
+                System.out.println("su numero no es primo");
+            }
+            //si el nuestro divisor es igual al numero que insertamos este va a decir//
+            //si es primo o no//
         }
-
-
+        in.close();
     }
-
-
 }
